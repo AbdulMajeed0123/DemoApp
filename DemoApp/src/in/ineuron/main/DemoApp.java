16514412dd98a16d80b339c7dd6440a1ca969ec8
Abdul <majeedph20@gmail.com>
2023-03-20 22:44:42 +0530

    First Commit

---
+++ DemoApp/src/in/ineuron/main/DemoApp.java
@@ -0,0 +1,20 @@
+package in.ineuron.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		
+		int [] a = {10,20,30,40};
+		for (int ele :a)
+		{
+			System.out.println(ele);
+		}
+		
+		String name = "dhoni";
+		int count = name.toUpperCase().length();
+		System.out.println("the length of the String is :" + count);
+	
+	}
+	
+
+}
